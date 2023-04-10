b295a7a9c3c95399fbd56f58881fb213e7e606e1
AlexF <alexfulleringer@gmail.com>
2023-04-10 14:36:38 -0400

    Adding option to run our attacked_alexnet

--- describe_neurons.py
+++ describe_neurons.py
@@ -17,7 +17,7 @@ parser.add_argument("--clip_model", type=str, default="ViT-B/16",
 parser.add_argument("--target_model", type=str, default="resnet50", 
                    help=""""Which model to dissect, supported options are pretrained imagenet models from
                         torchvision and resnet18_places""")
-parser.add_argument("--target_layers", type=str, default=["features0", "features3", "features6", "features8", "features10"],
+parser.add_argument("--target_layers", type=str, default=["features[0]", "features[3]", "features[6]", "features[8]", "features[10]"],
                     help="Which layer activations to look at. Following the naming scheme of the PyTorch module used")
 parser.add_argument("--d_probe", type=str, default="broden", 
                     choices = ["imagenet_broden", "cifar100_val", "imagenet_val", "broden", "imagenet_broden"])
